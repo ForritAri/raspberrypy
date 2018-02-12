2ded86240b0c06901e99ff8e12008b4c81591ac3
Ari Vésteinsson <forritari@outlook.com>
2018-02-12 23:35:27 +0000

    minor change

--- camera.py
+++ camera.py
@@ -1,6 +1,11 @@
 import picamera
+from time import sleep
 
 
 with picamera.PiCamera() as camera:
     camera.vflip = True
+    # camera.start_preview()
+    # sleep(10)
+    # camera.stop_preview()
+    # camera.exposure_mode = 'night'
     camera.capture('/home/pi/Desktop/image.jpg')
